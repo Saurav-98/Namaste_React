d8d6ad792e7d69f5f0a776a67cfb1719b3c1bd00
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-08-22 01:08:05 +0530

    Episode 1 The Inception Completed

--- 01-Inception/HomeWork.md
+++ 01-Inception/HomeWork.md
@@ -1,3 +1,7 @@
 ## What is CDN?
 
 ## What is CrossOrigin in CDN?
+
+### Q1 - Why React is a Library not a Framework?
+
+### Q2 -
--- 01-Inception/script.js
+++ 01-Inception/script.js
@@ -1,9 +1,42 @@
-const heading2 = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello, World! from React."
-);
+// Create the Structure
+
+// <div id='parent'>
+//   <div id="child">
+//     <h1>I am an H! TAG</h1>
+//     <h2>I am an H! TAG</h2>
+//   </div>
+// </div>
+
+const parent = React.createElement("div", { id: "parent" }, [
+  ,
+  [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement(
+        "h1",
+        { class: "heading1" },
+        "I am a React Heading One"
+      ),
+      React.createElement(
+        "h2",
+        { class: "heading2" },
+        "I am a React Heading Two"
+      ),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement(
+        "h1",
+        { class: "heading1" },
+        "I am a React Heading One"
+      ),
+      React.createElement(
+        "h2",
+        { class: "heading2" },
+        "I am a React Heading Two"
+      ),
+    ]),
+  ],
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading2);
+root.render(parent);
